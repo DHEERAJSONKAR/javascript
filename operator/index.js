50c8aa50f396a578196ca5db8057e52433a8a311
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-07-17 09:47:14 +0530

    17/07/2025
    first commit

---
+++ operator/index.js
@@ -0,0 +1,25 @@
+console.log(2+2);
+console.log(2-5);
+console.log(3*8);
+console.log(10/2);   // this is a arithmetic operator 
+
+var a = 10;
+// addition assignment
+console.log(a+=10);
+
+// subtraction assignment
+
+console.log(a -=5);
+console.log(a*=2);
+console.log(a/=10);
+
+
+
+// comparsion operator
+var b=2;
+var c ='2';
+// console.log(b==c); //true
+// console.log(b===c); // false because data type not same 
+
+console.log(b!=c); //false
+console.log(b!==c); // true
