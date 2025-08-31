85afd6e859e9de51bf0473c15a44ac4be8069067
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-08-31 12:30:54 +0530

    Add JavaScript file to demonstrate data types and their behavior

---
+++ datatypes/datatypes.js
@@ -0,0 +1,12 @@
+// data types in javascript
+// primitive data types -> aisi sari value jinko copy karne per tumhe ak real value mil jaye
+// string, numeber, null, undefinde, boolean, bigint, symbol
+// reference data types -> inko copy karne per  real value nhi milige  but apko reference milta hai parent ka
+// array, object, function
+
+let a =12;
+let b = a;
+console.log(a)
+console.log(b);
+a = a+2;
+console.log(a);
