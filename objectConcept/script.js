4bec100d52253f235e0c506cea0f3023ccf12e33
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-10-20 06:32:44 +0530

    Add initial implementation of object and DOM manipulation examples

---
+++ objectConcept/script.js
@@ -0,0 +1,8 @@
+let obj = {
+    name: "Sample Object",
+    value: 42,
+    age: 18
+}
+console.log("Object Name:", obj.name);
+console.log("Object Value:", obj.value);
+console.log("Object Age:", obj.age);
