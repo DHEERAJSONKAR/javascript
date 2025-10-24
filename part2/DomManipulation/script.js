f66666f15949931f8d54fdf10c6a692402bfbf6d
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-10-24 18:09:45 +0530

    Update link text for download button in HTML

--- part2/DomManipulation/script.js
+++ part2/DomManipulation/script.js
@@ -7,5 +7,10 @@
 // event listners
 
 let abcd = document.querySelector("h1");
-abcd.inner = "Hello, developer dheeraj";
+abcd.innerHTML = "<i>hello boy</>";
+console.dir(abcd); 
+
+let a = document.querySelector("a");
+a.href="https://www.google.com";
+console.log(a);
  
