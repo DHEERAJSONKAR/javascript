7b3bc789026fcdb29fade77280c84f67f619a700
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-10-26 05:15:59 +0530

    Comment out unused anchor tag and update image source in HTML; remove href attribute from Facebook link in JavaScript

--- part2/DomManipulation/script.js
+++ part2/DomManipulation/script.js
@@ -10,7 +10,15 @@ let abcd = document.querySelector("h1");
 abcd.innerHTML = "<i>hello boy</>";
 console.dir(abcd); 
 
-let a = document.querySelector("a");
-a.href="https://www.google.com";
-console.log(a);
+// let a = document.querySelector("a");
+// a.href="https://www.google.com";
+// console.log(a);
+
+let img = document.querySelector("img")
+img.setAttribute("src","https://images.unsplash.com/file-1715651741414-859baba4300dimage?auto=format&fit=crop&q=60&w=416&dpr=2")
  
+// let a = document.querySelector("a");
+// console.log(a.getAttribute("href")); 
+
+let a = document.querySelector("a");
+a.removeAttribute("href"); 
