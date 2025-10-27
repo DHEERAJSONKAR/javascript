3d17a48729766f6418165ee04c57b5517d661845
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-10-27 05:30:20 +0530

    Refactor HTML and JavaScript: Comment out unused elements and scripts; add CSS class to header

--- part2/DomManipulation/script.js
+++ part2/DomManipulation/script.js
@@ -6,19 +6,34 @@
 // attribute
 // event listners
 
-let abcd = document.querySelector("h1");
-abcd.innerHTML = "<i>hello boy</>";
-console.dir(abcd); 
+// let abcd = document.querySelector("h1");
+// abcd.innerHTML = "<i>hello boy</>";
+// console.dir(abcd); 
 
 // let a = document.querySelector("a");
 // a.href="https://www.google.com";
 // console.log(a);
 
-let img = document.querySelector("img")
-img.setAttribute("src","https://images.unsplash.com/file-1715651741414-859baba4300dimage?auto=format&fit=crop&q=60&w=416&dpr=2")
+// let img = document.querySelector("img")
+// img.setAttribute("src","https://images.unsplash.com/file-1715651741414-859baba4300dimage?auto=format&fit=crop&q=60&w=416&dpr=2")
  
 // let a = document.querySelector("a");
 // console.log(a.getAttribute("href")); 
 
-let a = document.querySelector("a");
-a.removeAttribute("href"); 
+// let a = document.querySelector("a");
+// a.removeAttribute("href"); 
+
+// create element
+// append/prepend jaha bhi element chahiye waha 
+
+// let h2 = document.createElement("h2");
+// h2.textContent = "this is a new h2 tag"; 
+// document.querySelector("body").append(h2);
+// console.log(h2);
+
+let h1 = document.querySelector("h1");
+// h1.style.color = "red";
+// h1.style.backgroundColor = "black"; 
+// h1.style.textAlign = "center";
+// h1.style.fontFamily="Gilroy"
+h1.classList.add("human");
